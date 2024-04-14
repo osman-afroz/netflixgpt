fc066ff00d9d0f1dd0727b4b37f7eeadb07b3a94
osman-afroz <ma.osmanafroz@gmail.com>
2024-04-14 18:06:41 +0530

    Few changes added

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'
+
+const Login = () => {
+            return(
+               <>
+                <div>
+                    <Header />
+                </div>
+                <div>
+                    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/3417bf9a-0323-4480-84ee-e1cb2ff0966b/IN-en-20240408-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
+                </div>
+               
+               </>
+            )
+}
+
+export default Login
