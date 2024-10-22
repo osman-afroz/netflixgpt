2d5a6592444d072bb209dabb29cd32f6d7b82fbd
osman-afroz <ma.osmanafroz@gmail.com>
2024-10-22 16:42:59 +0530

    saturday changes 19-10-2024

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,12 +1,12 @@
 export const checkValidData = (email, password,name) => {
-    const isNameValid =/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name)
+  // const isNameValid =/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name)
   const isEmailValid =
     /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
 
-// if(!isNameValid) return "Full Name is not valid "
+  // if(!isNameValid) return "Full Name is not valid "
   if (!isEmailValid) return "Email ID is not Valid";
   if (!isPasswordValid) return "Password ID is not Valid";
 
