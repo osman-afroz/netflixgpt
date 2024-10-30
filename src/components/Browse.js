88e29d3dd52220813c4b905690fc92ddf8b7780a
osman-afroz <ma.osmanafroz@gmail.com>
2024-10-30 15:00:36 +0530

    Added componenets for browse movie links 30/10/2024

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react';
-import Header from './Header';
-import { API_OPTIONS } from '../utils/constants';
-import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../utils/movieSlice';
+import React from "react";
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-
-  const dispatch = useDispatch();
-  const getNowPlayingMovies= async ()=>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json.results)
-    dispatch(addNowPlayingMovies(json.results))
-  }
-  useEffect(()=>{
-    getNowPlayingMovies()
-  },[])
+  useNowPlayingMovies();
   return (
     <div>
-      <Header/>
+      <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
+      {/* 
+      MainContainer
+           -VideoBackground 
+           - VideoTitlr
+         SecondaryContainer
+           - MovieList * n
+              - Cards * n
+        */}
     </div>
   );
-}
+};
 
 export default Browse;
